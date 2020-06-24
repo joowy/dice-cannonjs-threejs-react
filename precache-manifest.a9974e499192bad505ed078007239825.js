4aa4cd708f631b516e9d9cf4e72b05c630bb854a
DooterMcDooter <46057052+DooterMcDooter@users.noreply.github.com>
2020-06-23 22:33:11 -0400

    Updates

---
+++ precache-manifest.a9974e499192bad505ed078007239825.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61fb6f629015464938f193a1c1749ba3",
+    "url": "/dice-cannonjs-threejs-react/index.html"
+  },
+  {
+    "revision": "9783cd19aa5dc1846353",
+    "url": "/dice-cannonjs-threejs-react/static/css/main.07547fd2.chunk.css"
+  },
+  {
+    "revision": "e84c02105ee41ca5b241",
+    "url": "/dice-cannonjs-threejs-react/static/js/2.05ef30f5.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/dice-cannonjs-threejs-react/static/js/2.05ef30f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45cffc5fe536e8a87e2e",
+    "url": "/dice-cannonjs-threejs-react/static/js/3.2d70f6c5.chunk.js"
+  },
+  {
+    "revision": "9783cd19aa5dc1846353",
+    "url": "/dice-cannonjs-threejs-react/static/js/main.0015f4fe.chunk.js"
+  },
+  {
+    "revision": "ba973e6e5d495546e0b0",
+    "url": "/dice-cannonjs-threejs-react/static/js/runtime-main.8ea042d5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/777mlHg/dice-cannonjs-threejs-react/precache-manifest.90a6e9c173822752c0c7ad3eb9cb0c36.js"
+  "/dice-cannonjs-threejs-react/precache-manifest.a9974e499192bad505ed078007239825.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/777mlHg/dice-cannonjs-threejs-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dice-cannonjs-threejs-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
